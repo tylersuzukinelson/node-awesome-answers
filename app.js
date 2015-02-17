c7dd3848fef1eac3dc417a77c24d7d1fa2eb4091
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-17 11:15:30 -0800

    Set up app to display an index view with jade

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
+var jade = require("jade");
+
+app.engine("jade", jade.__express);
+app.set("view engine", "jade");
 
 app.get("/", function(request, response) {
-  response.end("Welcome to Node.js");
+  response.render("index");
 });
 
 console.log("Server has started. Listening on Port 5000.");
