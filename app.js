45def26b42e51d326e950d4512d5350b601ad32e
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-17 11:39:53 -0800

    Set up the app to use a layout view (npm install --save express-layout)

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var app = express();
+var layouts = require("express-layout");
 var jade = require("jade");
-
-// Middleware
-app.use(express.static("public")); // where to store the static files
 
 app.engine("jade", jade.__express);
 app.set("view engine", "jade");
+
+// Middleware
+app.use(express.static("public")); // where to store the static files
+app.use(layouts());
+app.set("layout", "app_layout");
 
 app.get("/", function(request, response) {
   response.render("index");
