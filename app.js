ab186a84a97be0282f3d22756adc3fc4ed718b62
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-17 14:55:53 -0800

    Moved questions#new to from app.js to controller

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var layouts = require("express-layout");
 var jade = require("jade");
+var app = express();
 
 app.engine("jade", jade.__express);
 app.set("view engine", "jade");
@@ -11,12 +12,11 @@ app.use(express.static("public")); // where to store the static files
 app.use(layouts());
 app.set("layout", "app_layout");
 
+questions_controller = require('./controllers/questions');
+questions_controller(app);
+
 app.get("/", function(request, response) {
   response.render("index");
-});
-
-app.get("/questions/new", function(req, res) {
-  res.render("questions/new");
 });
 
 console.log("Server has started. Listening on Port 5000.");
---
+++ controllers/questions.js
@@ -0,0 +1,5 @@
+module.exports = function(app) {
+  app.get("/questions/new", function(req, res) {
+    res.render("questions/new");
+  });
+}
